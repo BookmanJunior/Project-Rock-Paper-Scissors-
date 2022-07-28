3e221551bb751287ef42c08803b066210c2bab0e
Gulov Azam <gulovazam@gmail.com>
2022-07-28 12:36:02 +0500

    Added function that checks first player to 5 points

--- script.js
+++ script.js
@@ -1,54 +1,72 @@
+// Global variables
 const signs = ["Rock", "Paper", "Scissors"];
 let playerScore = 0;
 let computerScore = 0;
+let playerChoice;
 
-const buttons = document.querySelector('.signs');
-const result = document.querySelector('.result h3');
+const buttons = document.querySelector(".signs");
+const result = document.querySelector(".result h3");
+const restartBtn = document.querySelector(".resetBtn");
+const displayPlayerScore = document.getElementById("playerScore");
+const displayComputerScore = document.getElementById("computerScore");
 
 // Event listeners
-buttons.addEventListener('click', playRound);
+buttons.addEventListener("click", getPlayerChoice);
+buttons.addEventListener("click", playGame);
+restartBtn.addEventListener("click", () => {
+  playerScore = 0;
+  computerScore = 0;
+  displayPlayerScore.textContent = `Your Score: ${playerScore}`;
+  displayComputerScore.textContent = `Computer Score:  ${computerScore}`;
+});
 
-
-// returns random number 
+// returns random number
 function getComputerChoice() {
-    randomChoice = Math.floor(Math.random() * 3);
-    return randomChoice;
+  randomChoice = Math.floor(Math.random() * 3);
+  return randomChoice;
 }
 
+// Get players input
+function getPlayerChoice(e) {
+  let button = e.target.textContent;
+  switch (button) {
+    case "Rock":
+      return (playerChoice = 0);
+    case "Paper":
+      return (playerChoice = 1);
+    case "Scissors":
+      return (playerChoice = 2);
+  }
+}
 
-function playRound(e) {
-    const playerSelection = e.target.id;
-    const computerSelection = getComputerChoice();
-    
-    // If either selection is greater than the other selection they win. Except in a few cases.
-    if (playerSelection === 2 && computerSelection === 0) {
-        computerScore++;
-        result.textContent = `You Lose! ${signs[computerSelection]} beats ${signs[playerSelection]}`;
-    } else if (playerSelection === 0 && computerSelection === 2) {
-        playerScore++;
-        result.textContent =  `You Win! ${signs[playerSelection]} beats ${signs[computerSelection]}`;
-    } else if (playerSelection > computerSelection) {
-        playerScore++
-        result.textContent =  `You Win! ${signs[playerSelection]} beats ${signs[computerSelection]}`;
-    } else if (playerSelection < computerSelection) {
-        computerScore++;
-        result.textContent = `You Lose! ${signs[computerSelection]} beats ${signs[playerSelection]}`;
-    } else {
-        result.textContent = "It's a tie!";
-    }
+function playRound(playerSelection, computerSelection) {
+  // If either selection is greater than the other they win. Except in a few cases.
 
-};
+  // Check if either of players reached 5 points.
+if (playerSelection === 2 && computerSelection === 0) {
+    computerScore++;
+    displayComputerScore.textContent = `Computer's Score: ${computerScore}`;
+    result.textContent = `You Lose! ${signs[computerSelection]} beats ${signs[playerSelection]}`;
+} else if (playerSelection === 0 && computerSelection === 2) {
+    playerScore++;
+    displayPlayerScore.textContent = `Your Score: ${playerScore}`;
+    result.textContent = `You Win! ${signs[playerSelection]} beats ${signs[computerSelection]}`;
+} else if (playerSelection > computerSelection) {
+    playerScore++;
+    displayPlayerScore.textContent = `Your Score: ${playerScore}`;
+    result.textContent = `You Win! ${signs[playerSelection]} beats ${signs[computerSelection]}`;
+} else if (playerSelection < computerSelection) {
+    computerScore++;
+    displayComputerScore.textContent = `Computer's Score: ${computerScore}`;
+    result.textContent = `You Lose! ${signs[computerSelection]} beats ${signs[playerSelection]}`;
+} else {
+    result.textContent = "It's a tie!";
+}
+}
 
-// Remove for now.
-// function game() {
-//     for (i = 0; i < 5; i++) {
-//         console.log(playRound())
-//     }
-//     if (computerScore > playerScore) {
-//         console.log("Computer wins in the best of 5!")
-//     } else {
-//         console.log("You win in the best of 5!")
-//     }
-// }
-
-
+function playGame() {
+  playRound(
+    (playerSelection = playerChoice),
+    (computerSelection = getComputerChoice())
+  );
+}
