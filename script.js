70cfa8411f2446b8d0759d4c0923c9a13e6895ab
Gulov Azam <gulovazam@gmail.com>
2022-07-28 20:48:50 +0500

    Organised code to be more readable

--- script.js
+++ script.js
@@ -1,70 +1,74 @@
 // Global variables
 const signs = ["Rock", "Paper", "Scissors"];
-let playerScore = 0;
-let computerScore = 0;
-
 const buttons = document.querySelector(".signs");
 const result = document.querySelector(".result h2");
 const restartBtn = document.querySelector(".restartBtn");
 const displayPlayerScore = document.getElementById("playerScore");
 const displayComputerScore = document.getElementById("computerScore");
-const playerSignImage = document.querySelector(".player-sign")
-const computerSignImage = document.querySelector(".computer-sign")
+const playerSignImage = document.querySelector(".player-sign");
+const computerSignImage = document.querySelector(".computer-sign");
+let playerScore = 0;
+let computerScore = 0;
 
-
-buttons.addEventListener('click', playRound);
-
+buttons.addEventListener("click", playRound);
 // Restart game
-restartBtn.addEventListener('click', () => {
+restartBtn.addEventListener("click", () => {
   // Reset Score
   playerScore = 0;
   computerScore = 0;
-  result.textContent = 'Rock, Paper or Scissors?';
+  result.textContent = "Rock, Paper or Scissors?";
   displayPlayerScore.textContent = `Score: ${playerScore}`;
   displayComputerScore.textContent = `Score: ${computerScore}`;
 
   // Reset sign images
-  playerSignImage.src = 'images/Rock.png'
-  computerSignImage.src = 'images/Rock.png'
+  playerSignImage.src = "images/Rock.png";
+  computerSignImage.src = "images/Rock.png";
+
+  // Hide restart button
+  restartBtn.style.display = 'none';
 });
 
-
 function playRound(e) {
-    const playerSelection = parseInt(e.target.id)
-    console.log(e.target)
+  if (e.target.type === "submit") {
+    // Prevents clicking in-between buttons
+    const playerSelection = parseInt(e.target.id);
     // Change players sign
     playerSignImage.src = `images/${signs[playerSelection]}.png`;
-    const computerSelection = getComputerChoice()
+    const computerSelection = getComputerChoice();
 
-    // Check if either of players reached 5 points.
+    // Show restart button
+    restartBtn.style.display = 'initial';
+
+    // Determine rounds winner;
     if (playerSelection === 2 && computerSelection === 0) {
-        incrementComputerScore(playerSelection, computerSelection);
+      incrementComputerScore(playerSelection, computerSelection);
     } else if (playerSelection === 0 && computerSelection === 2) {
-        incrementPlayerScore(playerSelection, computerSelection);
+      incrementPlayerScore(playerSelection, computerSelection);
     } else if (playerSelection > computerSelection) {
-        incrementPlayerScore(playerSelection, computerSelection);
+      incrementPlayerScore(playerSelection, computerSelection);
     } else if (playerSelection < computerSelection) {
-        incrementComputerScore(playerSelection, computerSelection);
+      incrementComputerScore(playerSelection, computerSelection);
     } else {
-        result.textContent = "It's a tie!";
+      result.textContent = "It's a tie!";
     }
+  }
 }
 
 // returns random number
 function getComputerChoice() {
-    randomChoice = Math.floor(Math.random() * 3);
-    computerSignImage.src = `images/${signs[randomChoice]}.png`
-    return randomChoice;
+  randomChoice = Math.floor(Math.random() * 3);
+  computerSignImage.src = `images/${signs[randomChoice]}.png`;
+  return randomChoice;
 }
 
 function incrementPlayerScore(playerChoice, computerChoice) {
-    playerScore++;
-    displayPlayerScore.textContent = `Score: ${playerScore}`;
-    result.textContent = `You Win! ${signs[playerChoice]} beats ${signs[computerChoice]}`;
+  playerScore++;
+  displayPlayerScore.textContent = `Score: ${playerScore}`;
+  result.textContent = `You Win! ${signs[playerChoice]} beats ${signs[computerChoice]}`;
 }
 
 function incrementComputerScore(playerChoice, computerChoice) {
-    computerScore++;
-    displayComputerScore.textContent = `Score: ${computerScore}`;
-    result.textContent = `You Lose! ${signs[computerChoice]} beats ${signs[playerChoice]}`
+  computerScore++;
+  displayComputerScore.textContent = `Score: ${computerScore}`;
+  result.textContent = `You Lose! ${signs[computerChoice]} beats ${signs[playerChoice]}`;
 }
